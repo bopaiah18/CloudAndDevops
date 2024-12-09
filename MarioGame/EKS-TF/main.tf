f0f22ff9eb8cfab4aee4b64731926a61bbc7c6ed
Gowtham Bopaiah M G <72348327+bopaiah18@users.noreply.github.com>
2024-12-09 18:30:00 +0530

    Update main.tf
    Main file for terraform infrastructure

--- MarioGame/EKS-TF/main.tf
+++ MarioGame/EKS-TF/main.tf
@@ -1,1 +1,16 @@
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+module "iam" {
+  source = "./modules/iam"
+}
+
+module "eks" {
+  source      = "./modules/eks"
+  vpc_id      = module.vpc.vpc_id
+  subnet_ids  = module.vpc.public_subnet_ids
+  cluster_iam_role_arn = module.iam.cluster_iam_role_arn
+  node_iam_role_arn    = module.iam.node_iam_role_arn
+}
 
